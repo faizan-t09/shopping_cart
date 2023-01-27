8666b383415be4b63651d60b1a2ff81e55a3b6d5
faizan-majgaonkar <faizan.majgaonkar@torinit.ca>
2023-01-27 12:31:10 +0530

    Debounced validate onChange

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -21,10 +21,47 @@ export const NewItemForm: React.FC<newItemProps> = ({
     wishlisted: false,
   });
 
+  const [error, setError] = useState({ title: "", price: "", imgsrc: "" });
+
+  const validate = () => {
+    setError((curr) => {
+      let currentErrors = {
+        title: "",
+        price: "",
+        imgsrc: "",
+      };
+
+      if (!form.title) {
+        currentErrors.title = "Title is required";
+      } else if (form.title.length < 3) {
+        currentErrors.title = "Title must be longer than 3 chars";
+      }
+
+      if (!form.price) {
+        currentErrors.price = "Price is required";
+      } else if (Number(form.price) <= 0) {
+        currentErrors.price = "Price must be greater than 0";
+      }
+
+      if (!form.imgsrc) {
+        currentErrors.imgsrc = "Image Url is required";
+      }
+
+      return currentErrors;
+    });
+  };
+
+  //Debouncing validation 
+  const timeOutId = useRef<ReturnType<typeof setTimeout> | null>(null);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setForm((prev: itemType) => {
       return { ...prev, [event.target.name]: event.target.value };
     });
+    timeOutId.current && clearTimeout(timeOutId.current);
+    timeOutId.current = setTimeout(() => {
+      validate();
+      console.log("onChange called and validated");
+    }, 2500);
   };
 
   const clearForm = () => {
@@ -41,9 +78,16 @@ export const NewItemForm: React.FC<newItemProps> = ({
 
   const addItem = (event: React.FormEvent) => {
     event.preventDefault();
-    addNewItem(form);
-    clearForm();
-    navigate("/");
+    validate();
+    setError((curr) => {
+      //checking if there is any error in the error object
+      if (!Boolean(curr.title || curr.price || curr.imgsrc)) {
+        addNewItem(form);
+        clearForm();
+        navigate("/");
+      }
+      return curr;
+    });
   };
 
   const inpRef = useRef<HTMLInputElement>(null);
@@ -67,6 +111,7 @@ export const NewItemForm: React.FC<newItemProps> = ({
               onChange={handleChange}
             ></input>
           </div>
+          {error.title}
           <div>
             <label>Description : </label>
             <input
@@ -87,6 +132,7 @@ export const NewItemForm: React.FC<newItemProps> = ({
               onChange={handleChange}
             ></input>
           </div>
+          {error.price}
           <div>
             <label>Image Url : </label>
             <input
@@ -97,6 +143,7 @@ export const NewItemForm: React.FC<newItemProps> = ({
               onChange={handleChange}
             ></input>
           </div>
+          {error.imgsrc}
           <div className="form-actions">
             <button
               onClick={() => {
@@ -113,7 +160,12 @@ export const NewItemForm: React.FC<newItemProps> = ({
             >
               Clear
             </button>
-            <button type="submit">Submit</button>
+            <button
+              type="submit"
+              disabled={Boolean(error.title || error.price || error.imgsrc)}
+            >
+              Submit
+            </button>
           </div>
         </form>
       </div>
